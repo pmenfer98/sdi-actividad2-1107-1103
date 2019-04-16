5d43bd795a51d68dff21d234b232752c3ad4f718
Pablo Menéndez Fernández <uo257971@uniovi.es>
2019-04-16 21:46:33 +0200

    Crea proyecto test

---
+++ sdi-actividad2-1107-1103-test/src/test/Sdi211071103LabSpringApplicationTests.java
@@ -0,0 +1,70 @@
+package test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Sdi211071103LabSpringApplicationTests {
+	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
+	// automáticas)):
+	static String PathFirefox64 = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver022 = "C:\\Users\\Pablo\\Desktop\\SDI\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
+	// Común a Windows y a MACOSX
+	static WebDriver driver = getDriver(PathFirefox64, Geckdriver022);
+	static String URL = "http://localhost:8081";
+
+	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		driver.navigate().to(URL);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.manage().deleteAllCookies();
+	}
+
+	@BeforeClass
+	static public void begin() {
+	}
+
+	// Al finalizar la última prueba
+	@AfterClass
+	static public void end() {
+		// Cerramos el navegador al finalizar las pruebas
+		driver.quit();
+	}
+
+	@Test
+	public void prueba1() throws Exception {
+		driver.get(URL + "/signup");
+		driver.findElement(By.name("name")).click();
+		driver.findElement(By.name("name")).clear();
+		driver.findElement(By.name("name")).sendKeys("Juan Carlos");
+		driver.findElement(By.name("lastName")).click();
+		driver.findElement(By.name("lastName")).clear();
+		driver.findElement(By.name("lastName")).sendKeys("Álvarez García");
+		driver.findElement(By.name("email")).click();
+		driver.findElement(By.name("email")).clear();
+		driver.findElement(By.name("email")).sendKeys("UO123456@uniovi.es");
+		driver.findElement(By.name("password")).click();
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys("ContraseñaContra");
+		driver.findElement(By.name("passwordConfirm")).click();
+		driver.findElement(By.name("passwordConfirm")).clear();
+		driver.findElement(By.name("passwordConfirm"))
+				.sendKeys("ContraseñaContra");
+		driver.findElement(By.id("registerButton")).click();
+	}
+}
