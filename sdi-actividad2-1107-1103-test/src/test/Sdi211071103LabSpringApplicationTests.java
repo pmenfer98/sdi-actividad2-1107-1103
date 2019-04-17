0d7485e8d87cafb0ebf009a990b1cc8c77fae330
Pablo <UO258082@uniovi.es>
2019-04-17 10:33:13 +0200

    Metadatos y UTF-8

--- sdi-actividad2-1107-1103-test/src/test/Sdi211071103LabSpringApplicationTests.java
+++ sdi-actividad2-1107-1103-test/src/test/Sdi211071103LabSpringApplicationTests.java
@@ -10,11 +10,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Sdi211071103LabSpringApplicationTests {
-	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
-	// automáticas)):
+	// En Windows (Debe ser la versiÃ³n 65.0.1 y desactivar las actualizacioens
+	// automÃ¡ticas)):
 	static String PathFirefox64 = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
-	static String Geckdriver022 = "C:\\Users\\Pablo\\Desktop\\SDI\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
-	// Común a Windows y a MACOSX
+	static String Geckdriver022 = "C:\\Users\\Pablo\\Desktop\\SDI\\PL-SDI-SesiÃ³n5-material\\geckodriver024win64.exe";
+	// ComÃºn a Windows y a MACOSX
 	static WebDriver driver = getDriver(PathFirefox64, Geckdriver022);
 	static String URL = "http://localhost:8081";
 
@@ -39,12 +39,18 @@ public class Sdi211071103LabSpringApplicationTests {
 	static public void begin() {
 	}
 
-	// Al finalizar la última prueba
+	// Al finalizar la Ãºltima prueba
 	@AfterClass
 	static public void end() {
 		// Cerramos el navegador al finalizar las pruebas
 		driver.quit();
 	}
+	
+	@Test
+	public void testUntitledTestCase() throws Exception {
+		
+	}
+
 
 	@Test
 	public void prueba1() throws Exception {
@@ -54,17 +60,22 @@ public class Sdi211071103LabSpringApplicationTests {
 		driver.findElement(By.name("name")).sendKeys("Juan Carlos");
 		driver.findElement(By.name("lastName")).click();
 		driver.findElement(By.name("lastName")).clear();
-		driver.findElement(By.name("lastName")).sendKeys("Álvarez García");
+		driver.findElement(By.name("lastName")).sendKeys("Ã�lvarez GarcÃ­a");
 		driver.findElement(By.name("email")).click();
 		driver.findElement(By.name("email")).clear();
 		driver.findElement(By.name("email")).sendKeys("UO123456@uniovi.es");
 		driver.findElement(By.name("password")).click();
 		driver.findElement(By.name("password")).clear();
-		driver.findElement(By.name("password")).sendKeys("ContraseñaContra");
+		driver.findElement(By.name("password")).sendKeys("ContraseÃ±aContra");
 		driver.findElement(By.name("passwordConfirm")).click();
 		driver.findElement(By.name("passwordConfirm")).clear();
 		driver.findElement(By.name("passwordConfirm"))
-				.sendKeys("ContraseñaContra");
+				.sendKeys("ContraseÃ±aContra");
 		driver.findElement(By.id("registerButton")).click();
+	}
+	
+	@Test
+	public void prueba10() throws Exception {
+			
 	}
 }
