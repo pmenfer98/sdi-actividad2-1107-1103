2e57d87614a64ad9d50f8de100bee63e25451eac
Pablo Menendez Fernandez <uo257971@uniovi.es>
2019-04-23 22:57:49 +0200

    Añadir oferta destacada terminado

--- sdi-actividad2-1107-1103/routes/rofertas.js
+++ sdi-actividad2-1107-1103/routes/rofertas.js
@@ -103,10 +103,13 @@ module.exports = function (app, swig, gestorBD) {
             nombre: req.body.nombre,
             detalles: req.body.detalles,
             precio: req.body.precio,
+            destacada: (req.body.destacada === "on"),
             propietario: req.session.user.email,
             fecha: dateString
         };
-        if (oferta.nombre === null || oferta.nombre === undefined || oferta.nombre === '' ||
+        if (oferta.destacada && req.session.user.dinero < 20) {
+            res.redirect("/ofertas/agregar?mensaje=No tienes suficiente dinero para destacar la oferta");
+        } else if (oferta.nombre === null || oferta.nombre === undefined || oferta.nombre === '' ||
             oferta.detalles === null || oferta.detalles === undefined || oferta.detalles === '' ||
             oferta.precio === null || oferta.precio === undefined || oferta.precio <= 0) {
             res.redirect("/ofertas/agregar?mensaje=Los campos no son validos");
@@ -117,198 +120,214 @@ module.exports = function (app, swig, gestorBD) {
                 if (id == null) {
                     res.redirect("/publicaciones?mensaje=Error al añadir oferta");
                 } else {
-                    res.redirect("/publicaciones?mensaje=Nueva oferta añadida");
-                }
-            });
-        }
-
-    });
-
-    app.get('/canciones/:genero/:id', function (req, res) {
-        var respuesta = 'id: ' + req.params.id + '<br>'
-            + 'Genero: ' + req.params.genero;
-        res.send(respuesta);
-    });
-
-    app.get("/tienda", function (req, res) {
-        let criterio = {
-            propietario: {
-                $ne: req.session.user.email
-            }
-        };
-        if (req.query.busqueda != null) {
-            //var word = "/^" + req.query.busqueda + "$/";
-            var word = req.query.busqueda;
-            criterio = {
-                nombre: {$regex: new RegExp(word, "i")},
-                propietario: {
-                    $ne: req.session.user.email
-                }
-            };
-        }
-        console.log("Objeto busqueda " + req.query.busqueda);
-        var pg = parseInt(req.query.pg); // Es String !!!
-        if (req.query.pg == null) { // Puede no venir el param
-            pg = 1;
-        }
-        gestorBD.obtenerOfertasPg(criterio, criterio, pg, function (ofertas, total) {
-            if (ofertas == null) {
-                res.send("Error al listar ");
-            } else {
-                var ultimaPg = total / 5;
-                if (total % 5 > 0) { // Sobran decimales
-                    ultimaPg = ultimaPg + 1;
-                }
-                var paginas = []; // paginas mostrar
-                for (var i = pg - 2; i <= pg + 2; i++) {
-                    if (i > 0 && i <= ultimaPg) {
-                        paginas.push(i);
-                    }
-                }
-                var respuesta = swig.renderFile('views/btienda.html',
-                    {
-                        user: req.session.user,
-                        ofertas: ofertas,
-                        paginas: paginas,
-                        actual: pg,
-                        busqueda: req.query.busqueda
-                    });
-                res.send(respuesta);
-            }
-        });
-    });
-
-    app.get('/cancion/modificar/:id', function (req, res) {
-        var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
-        gestorBD.obtenerOfertas(criterio, function (canciones) {
-            if (canciones == null) {
-                res.send(respuesta);
-                res.redirect("/identificarse" +
-                    "?mensaje=Email o password incorrecto" +
-                    "&tipoMensaje=alert-danger ");
-            } else {
-                var respuesta = swig.renderFile('views/bcancionModificar.html',
-                    {
-                        cancion: canciones[0]
-                    });
-                res.send(respuesta);
-            }
-        });
-    });
-
-    app.post('/cancion/modificar/:id', function (req, res) {
-        var id = req.params.id;
-        var criterio = {"_id": gestorBD.mongo.ObjectID(id)};
-        var cancion = {
-            nombre: req.body.nombre,
-            genero: req.body.genero,
-            precio: req.body.precio
-        }
-        if (req.body == null || req.genero == null || req.precio == null) {
-            res.redirect("/publicaciones" +
-                "?mensaje=Los datos proporcionados no son validos" +
-                "&tipoMensaje=alert-danger ");
-        } else {
-            if (req.body === '' || req.genero === '' || req.precio === '') {
-                res.redirect("/publicaciones" +
-                    "?mensaje=Los datos proporcionados no son validos" +
-                    "&tipoMensaje=alert-danger ");
-            } else {
-                gestorBD.modificarCancion(criterio, cancion, function (result) {
-                    if (result == null || result.length == 0) {
-                        res.redirect("/publicaciones" +
-                            "?mensaje=La cancion que se quiere modificar no existe" +
-                            "&tipoMensaje=alert-danger ");
-                    } else {
-                        paso1ModificarPortada(req.files, id, function (result) {
-                            if (result == null) {
-                                res.redirect("/publicaciones" +
-                                    "?mensaje=Error en la modificacion" +
-                                    "&tipoMensaje=alert-danger ");
-                            } else {
-                                res.send("Modificado");
-                            }
-                        });
-
-                    }
-                });
-            }
-        }
-    });
-
-
-    app.get("/publicaciones", function (req, res) {
-        var criterio = {propietario: req.session.user.email};
-        gestorBD.obtenerOfertas(criterio, function (ofertas) {
-            if (ofertas == null) {
-                res.send("Error al listar ");
-            } else {
-                var respuesta = swig.renderFile('views/bpublicacionespropias.html',
-                    {
-                        user: req.session.user,
-                        ofertas: ofertas
-                    });
-                res.send(respuesta);
-            }
-        });
-    });
-
-    app.get('/oferta/eliminar/:id', function (req, res) {
-        if (req.params.id === null || req.params.id === undefined || req.params.id === '') {
-            res.redirect("/publicaciones" +
-                "?mensaje=Error al eliminar la publicacion" +
-                "&tipoMensaje=alert-danger ");
-        } else {
-            var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
-            gestorBD.eliminarOferta(criterio, function (canciones) {
-                if (canciones == null) {
-                    res.redirect("/publicaciones" +
-                        "?mensaje=Error al eliminar la publicacion" +
-                        "&tipoMensaje=alert-danger ");
-                } else {
-                    res.redirect("/publicaciones");
-                }
-            });
-        }
-    });
-
-    app.get('/oferta/destacar/:id', function (req, res) {
-        if (req.params.id === null || req.params.id === undefined || req.params.id === '') {
-            res.redirect("/publicaciones" +
-                "?mensaje=El valor de la oferta no es valido" +
-                "&tipoMensaje=alert-danger ");
-        } else {
-            var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
-            if (req.session.user.dinero < 20) {
-                res.redirect("/publicaciones" +
-                    "?mensaje=No tienes suficiente dinero, necesitas 20€" +
-                    "&tipoMensaje=alert-danger ");
-            } else {
-                gestorBD.destacarOferta(criterio, function (ofertas) {
-                    if (ofertas == null) {
-                        res.redirect("/publicaciones" +
-                            "?mensaje=No se puede destacar esta publicacion" +
-                            "&tipoMensaje=alert-danger ");
-                    } else {
-                        gestorBD.usuarioDestaca({"email":req.session.user.email}, function (result) {
+                    if(oferta.destacada){
+                        gestorBD.usuarioDestaca({"email": req.session.user.email}, function (result) {
                             if (result == null) {
                                 res.redirect("/publicaciones" +
                                     "?mensaje=Error destacando la oferta" +
                                     "&tipoMensaje=alert-danger ");
                             } else {
-                                res.redirect("/publicaciones" +
-                                    "?mensaje=Publicacion destacada" +
-                                    "&tipoMensaje=alert-success ");
+                                req.session.user.dinero-=20;
+                                res.redirect("/publicaciones?mensaje=Nueva oferta añadida");
                             }
                         });
+                    }else {
+                        res.redirect("/publicaciones?mensaje=Nueva oferta añadida");
                     }
-                });
+                }
+            });
+        }
+    }
+)
+;
+
+app.get('/canciones/:genero/:id', function (req, res) {
+    var respuesta = 'id: ' + req.params.id + '<br>'
+        + 'Genero: ' + req.params.genero;
+    res.send(respuesta);
+});
+
+app.get("/tienda", function (req, res) {
+    let criterio = {
+        propietario: {
+            $ne: req.session.user.email
+        }
+    };
+    if (req.query.busqueda != null) {
+        //var word = "/^" + req.query.busqueda + "$/";
+        var word = req.query.busqueda;
+        criterio = {
+            nombre: {$regex: new RegExp(word, "i")},
+            propietario: {
+                $ne: req.session.user.email
             }
+        };
+    }
+    console.log("Objeto busqueda " + req.query.busqueda);
+    var pg = parseInt(req.query.pg); // Es String !!!
+    if (req.query.pg == null) { // Puede no venir el param
+        pg = 1;
+    }
+    gestorBD.obtenerOfertasPg(criterio, criterio, pg, function (ofertas, total) {
+        if (ofertas == null) {
+            res.send("Error al listar ");
+        } else {
+            var ultimaPg = total / 5;
+            if (total % 5 > 0) { // Sobran decimales
+                ultimaPg = ultimaPg + 1;
+            }
+            var paginas = []; // paginas mostrar
+            for (var i = pg - 2; i <= pg + 2; i++) {
+                if (i > 0 && i <= ultimaPg) {
+                    paginas.push(i);
+                }
+            }
+            var respuesta = swig.renderFile('views/btienda.html',
+                {
+                    user: req.session.user,
+                    ofertas: ofertas,
+                    paginas: paginas,
+                    actual: pg,
+                    busqueda: req.query.busqueda
+                });
+            res.send(respuesta);
         }
     });
+});
+
+app.get('/cancion/modificar/:id', function (req, res) {
+    var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+    gestorBD.obtenerOfertas(criterio, function (canciones) {
+        if (canciones == null) {
+            res.send(respuesta);
+            res.redirect("/identificarse" +
+                "?mensaje=Email o password incorrecto" +
+                "&tipoMensaje=alert-danger ");
+        } else {
+            var respuesta = swig.renderFile('views/bcancionModificar.html',
+                {
+                    cancion: canciones[0]
+                });
+            res.send(respuesta);
+        }
+    });
+});
+
+app.post('/cancion/modificar/:id', function (req, res) {
+    var id = req.params.id;
+    var criterio = {"_id": gestorBD.mongo.ObjectID(id)};
+    var cancion = {
+        nombre: req.body.nombre,
+        genero: req.body.genero,
+        precio: req.body.precio
+    }
+    if (req.body == null || req.genero == null || req.precio == null) {
+        res.redirect("/publicaciones" +
+            "?mensaje=Los datos proporcionados no son validos" +
+            "&tipoMensaje=alert-danger ");
+    } else {
+        if (req.body === '' || req.genero === '' || req.precio === '') {
+            res.redirect("/publicaciones" +
+                "?mensaje=Los datos proporcionados no son validos" +
+                "&tipoMensaje=alert-danger ");
+        } else {
+            gestorBD.modificarCancion(criterio, cancion, function (result) {
+                if (result == null || result.length == 0) {
+                    res.redirect("/publicaciones" +
+                        "?mensaje=La cancion que se quiere modificar no existe" +
+                        "&tipoMensaje=alert-danger ");
+                } else {
+                    paso1ModificarPortada(req.files, id, function (result) {
+                        if (result == null) {
+                            res.redirect("/publicaciones" +
+                                "?mensaje=Error en la modificacion" +
+                                "&tipoMensaje=alert-danger ");
+                        } else {
+                            res.send("Modificado");
+                        }
+                    });
+
+                }
+            });
+        }
+    }
+});
 
 
-};
+app.get("/publicaciones", function (req, res) {
+    var criterio = {propietario: req.session.user.email};
+    gestorBD.obtenerOfertas(criterio, function (ofertas) {
+        if (ofertas == null) {
+            res.send("Error al listar ");
+        } else {
+            var respuesta = swig.renderFile('views/bpublicacionespropias.html',
+                {
+                    user: req.session.user,
+                    ofertas: ofertas
+                });
+            res.send(respuesta);
+        }
+    });
+});
+
+app.get('/oferta/eliminar/:id', function (req, res) {
+    if (req.params.id === null || req.params.id === undefined || req.params.id === '') {
+        res.redirect("/publicaciones" +
+            "?mensaje=Error al eliminar la publicacion" +
+            "&tipoMensaje=alert-danger ");
+    } else {
+        var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+        gestorBD.eliminarOferta(criterio, function (canciones) {
+            if (canciones == null) {
+                res.redirect("/publicaciones" +
+                    "?mensaje=Error al eliminar la publicacion" +
+                    "&tipoMensaje=alert-danger ");
+            } else {
+                res.redirect("/publicaciones");
+            }
+        });
+    }
+});
+
+app.get('/oferta/destacar/:id', function (req, res) {
+    if (req.params.id === null || req.params.id === undefined || req.params.id === '') {
+        res.redirect("/publicaciones" +
+            "?mensaje=El valor de la oferta no es valido" +
+            "&tipoMensaje=alert-danger ");
+    } else {
+        var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+        if (req.session.user.dinero < 20) {
+            res.redirect("/publicaciones" +
+                "?mensaje=No tienes suficiente dinero, necesitas 20€" +
+                "&tipoMensaje=alert-danger ");
+        } else {
+            gestorBD.destacarOferta(criterio, function (ofertas) {
+                if (ofertas == null) {
+                    res.redirect("/publicaciones" +
+                        "?mensaje=No se puede destacar esta publicacion" +
+                        "&tipoMensaje=alert-danger ");
+                } else {
+                    gestorBD.usuarioDestaca({"email": req.session.user.email}, function (result) {
+                        if (result == null) {
+                            res.redirect("/publicaciones" +
+                                "?mensaje=Error destacando la oferta" +
+                                "&tipoMensaje=alert-danger ");
+                        } else {
+                            req.session.user.dinero-=20;
+                            res.redirect("/publicaciones" +
+                                "?mensaje=Publicacion destacada" +
+                                "&tipoMensaje=alert-success ");
+                        }
+                    });
+                }
+            });
+        }
+    }
+});
+
+
+}
+;
 
 function paso1ModificarPortada(files, id, callback) {
     if (files.portada != null) {
