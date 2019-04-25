0790ae6c2b00ed70a088eebb2da2c27440420693
Pablo <UO258082@uniovi.es>
2019-04-25 17:29:32 +0200

    Mostrar listado de ofertas de otros usuarios (API REST)

--- sdi-actividad2-1107-1103/routes/rapitienda.js
+++ sdi-actividad2-1107-1103/routes/rapitienda.js
@@ -1,81 +1,88 @@
-module.exports = function(app, gestorBD) {
-    app.get("/api/oferta", function(req, res) {
-        gestorBD.obtenerOfertas( {} , function(ofertas) {
+module.exports = function (app, gestorBD) {
+    app.get("/api/oferta", function (req, res) {
+
+        let criterioMongo = {
+            propietario: {
+                $ne: req.session.user.email
+            }
+        };
+
+        gestorBD.obtenerOfertas(criterioMongo, function (ofertas) {
             if (ofertas == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error: "se ha producido un error"
                 })
             } else {
                 res.status(200);
-                res.send( JSON.stringify(ofertas) );
+                res.send(JSON.stringify(ofertas));
             }
         });
     });
 
 
-    app.get("/api/oferta/:id", function(req, res) {
-        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
+    app.get("/api/oferta/:id", function (req, res) {
+        var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
 
-        gestorBD.obtenerOfertas(criterio,function(ofertas){
-            if ( ofertas == null ){
+        gestorBD.obtenerOfertas(criterio, function (ofertas) {
+            if (ofertas == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error: "se ha producido un error"
                 })
             } else {
                 res.status(200);
-                res.send( JSON.stringify(ofertas[0]) );
+                res.send(JSON.stringify(ofertas[0]));
             }
         });
     });
 
 
-    app.post("/api/autenticar/", function(req, res) {
+    app.post("/api/autenticar/", function (req, res) {
         var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
         var criterio = {
-            email : req.body.email,
-            password : seguro
+            email: req.body.email,
+            password: seguro
         }
 
-        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
             if (usuarios == null || usuarios.length == 0) {
                 res.status(401); // Unauthorized
                 res.json({
-                    autenticado : false
+                    autenticado: false
                 })
             } else {
                 var token = app.get('jwt').sign(
-                    {usuario: criterio.email , tiempo: Date.now()/1000},
+                    {usuario: criterio.email, tiempo: Date.now() / 1000},
                     "secreto");
                 res.status(200);
                 res.json({
-                    autenticado : true,
-                    token : token
+                    autenticado: true,
+                    token: token
                 })
             }
 
         });
     });
 
-    app.post("/api/cancion", function(req, res) {
+    app.post("/api/cancion", function (req, res) {
         var cancion = {
-            nombre : req.body.nombre,
-            genero : req.body.genero,
-            precio : req.body.precio,
+            nombre: req.body.nombre,
+            genero: req.body.genero,
+            precio: req.body.precio,
         }
-        gestorBD.insertarOferta(cancion, function(id){
+        gestorBD.insertarOferta(cancion, function (id) {
             if (id == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error: "se ha producido un error"
                 })
             } else {
                 res.status(201);
                 res.json({
-                    mensaje : "canción insertarda",
-                    _id : id
+                    mensaje: "canción insertarda",
+                    _id: id
                 })
             }
         });
@@ -83,50 +90,48 @@ module.exports = function(app, gestorBD) {
     });
 
 
+    app.delete("/api/cancion/:id", function (req, res) {
+        var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
 
-    app.delete("/api/cancion/:id", function(req, res) {
-        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
-
-        gestorBD.eliminarOferta(criterio,function(canciones){
-            if ( canciones == null ){
+        gestorBD.eliminarOferta(criterio, function (canciones) {
+            if (canciones == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error: "se ha producido un error"
                 })
             } else {
                 res.status(200);
-                res.send( JSON.stringify(canciones) );
+                res.send(JSON.stringify(canciones));
             }
         });
     });
 
-    app.put("/api/cancion/:id", function(req, res) {
+    app.put("/api/cancion/:id", function (req, res) {
 
-        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
 
         var cancion = {}; // Solo los atributos a modificar
-        if ( req.body.nombre != null)
+        if (req.body.nombre != null)
             cancion.nombre = req.body.nombre;
-        if ( req.body.genero != null)
+        if (req.body.genero != null)
             cancion.genero = req.body.genero;
-        if ( req.body.precio != null)
+        if (req.body.precio != null)
             cancion.precio = req.body.precio;
-        gestorBD.modificarCancion(criterio, cancion, function(result) {
+        gestorBD.modificarCancion(criterio, cancion, function (result) {
             if (result == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error: "se ha producido un error"
                 })
             } else {
                 res.status(200);
                 res.json({
-                    mensaje : "canción modificada",
-                    _id : req.params.id
+                    mensaje: "canción modificada",
+                    _id: req.params.id
                 })
             }
         });
     });
-
 
 
 };
