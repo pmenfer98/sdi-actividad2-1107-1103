c7988128de4a324fe9738fc79a3f616b9bf21d73
Pablo <UO258082@uniovi.es>
2019-04-28 18:35:59 +0200

    Mostrar mensajes finalizado

--- sdi-actividad2-1107-1103/routes/rapitienda.js
+++ sdi-actividad2-1107-1103/routes/rapitienda.js
@@ -104,8 +104,8 @@ module.exports = function (app, gestorBD) {
     });
 
     app.get("/api/conversacion/oferta/:id", function (req, res) {
-        let criterioMongo = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
-        gestorBD.obtenerOfertas(criterio, function (ofertas) {
+        let criterioMongo = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+        gestorBD.obtenerOfertas(criterioMongo, function (ofertas) {
             if (ofertas == null) {
                 res.status(500);
                 res.json({
@@ -143,88 +143,88 @@ module.exports = function (app, gestorBD) {
                             ]
                         }
                     ]
-                }
+                };
+                gestorBD.obtenerMensajes(criterio, function (mensajes) {
+                    if (mensajes == null) {
+                        res.status(500);
+                        res.json({
+                            error: "se ha producido un error"
+                        })
+                    } else {
+                        res.status(200);
+                        res.send(JSON.stringify(mensajes));
+                    }
+                });
             }
-            gestorBD.obtenerMensajes(criterio, function (mensajes) {
-                if (mensajes == null) {
+        });
+    });
+
+        app.post("/api/cancion", function (req, res) {
+            var cancion = {
+                nombre: req.body.nombre,
+                genero: req.body.genero,
+                precio: req.body.precio,
+            }
+            gestorBD.insertarOferta(cancion, function (id) {
+                if (id == null) {
+                    res.status(500);
+                    res.json({
+                        error: "se ha producido un error"
+                    })
+                } else {
+                    res.status(201);
+                    res.json({
+                        mensaje: "canción insertarda",
+                        _id: id
+                    })
+                }
+            });
+
+        });
+
+
+        app.delete("/api/cancion/:id", function (req, res) {
+            var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
+
+            gestorBD.eliminarOferta(criterio, function (canciones) {
+                if (canciones == null) {
                     res.status(500);
                     res.json({
                         error: "se ha producido un error"
                     })
                 } else {
                     res.status(200);
-                    res.send(JSON.stringify(mensajes));
+                    res.send(JSON.stringify(canciones));
                 }
             });
         });
-    });
 
-    app.post("/api/cancion", function (req, res) {
-        var cancion = {
-            nombre: req.body.nombre,
-            genero: req.body.genero,
-            precio: req.body.precio,
-        }
-        gestorBD.insertarOferta(cancion, function (id) {
-            if (id == null) {
-                res.status(500);
-                res.json({
-                    error: "se ha producido un error"
-                })
-            } else {
-                res.status(201);
-                res.json({
-                    mensaje: "canción insertarda",
-                    _id: id
-                })
-            }
+        app.put("/api/cancion/:id", function (req, res) {
+
+            var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+
+            var cancion = {}; // Solo los atributos a modificar
+            if (req.body.nombre != null)
+                cancion.nombre = req.body.nombre;
+            if (req.body.genero != null)
+                cancion.genero = req.body.genero;
+            if (req.body.precio != null)
+                cancion.precio = req.body.precio;
+            gestorBD.modificarCancion(criterio, cancion, function (result) {
+                if (result == null) {
+                    res.status(500);
+                    res.json({
+                        error: "se ha producido un error"
+                    })
+                } else {
+                    res.status(200);
+                    res.json({
+                        mensaje: "canción modificada",
+                        _id: req.params.id
+                    })
+                }
+            });
         });
 
-    });
 
-
-    app.delete("/api/cancion/:id", function (req, res) {
-        var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)}
-
-        gestorBD.eliminarOferta(criterio, function (canciones) {
-            if (canciones == null) {
-                res.status(500);
-                res.json({
-                    error: "se ha producido un error"
-                })
-            } else {
-                res.status(200);
-                res.send(JSON.stringify(canciones));
-            }
-        });
-    });
-
-    app.put("/api/cancion/:id", function (req, res) {
-
-        var criterio = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
-
-        var cancion = {}; // Solo los atributos a modificar
-        if (req.body.nombre != null)
-            cancion.nombre = req.body.nombre;
-        if (req.body.genero != null)
-            cancion.genero = req.body.genero;
-        if (req.body.precio != null)
-            cancion.precio = req.body.precio;
-        gestorBD.modificarCancion(criterio, cancion, function (result) {
-            if (result == null) {
-                res.status(500);
-                res.json({
-                    error: "se ha producido un error"
-                })
-            } else {
-                res.status(200);
-                res.json({
-                    mensaje: "canción modificada",
-                    _id: req.params.id
-                })
-            }
-        });
-    });
-
-
-};
+    };
